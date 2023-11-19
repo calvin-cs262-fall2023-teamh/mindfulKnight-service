280cca5009e0a3eadd13d1078707bfc4da758e0f
CoreyC315 <122757429+CoreyC315@users.noreply.github.com>
2023-11-19 13:48:27 -0500

    Update mindfulKnight.js

--- mindfulKnight.js
+++ mindfulKnight.js
@@ -23,6 +23,11 @@ router.use(express.json());
 router.get('/', readHelloMessage);
 router.get('/users', readUsers);
 router.get('/fidget_toys', readFidgetToys);
+router.get('/user_activity', readUserActivity);
+router.get('/user_activity/:id', readUserActivityById);
+router.post('/user_activity', createUserActivity);
+router.put('/user_activity/:id', updateUserActivity);
+router.delete('/user_activity/:id', deleteUserActivity);
 
 app.use(router);
 app.listen(port, () => console.log(`Listening on port ${port}`));
@@ -66,7 +71,55 @@ function readFidgetToys(req, res, next) {
 }
 
 
+function readUserActivity(req, res, next) {
+  db.many('SELECT * FROM user_activity')
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
 
+function readUserActivityById(req, res, next) {
+  db.oneOrNone('SELECT * FROM user_activity WHERE activity_id=${id}', req.params)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function createUserActivity(req, res, next) {
+  db.one('INSERT INTO user_activity(user_id, toy_id, start_time, end_time) VALUES (${user_id}, ${toy_id}, ${start_time}, ${end_time}) RETURNING activity_id', req.body)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function updateUserActivity(req, res, next) {
+  db.oneOrNone('UPDATE user_activity SET user_id=${user_id}, toy_id=${toy_id}, start_time=${start_time}, end_time=${end_time} WHERE activity_id=${params.id} RETURNING activity_id', req)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function deleteUserActivity(req, res, next) {
+  db.oneOrNone('DELETE FROM user_activity WHERE activity_id=${id} RETURNING activity_id', req.params)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
 
 
 

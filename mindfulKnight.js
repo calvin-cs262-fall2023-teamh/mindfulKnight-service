90eedde42018451b615b18dd3f48b78d92949e47
CoreyC315 <122757429+CoreyC315@users.noreply.github.com>
2023-12-14 17:20:19 -0500

    Update mindfulKnight.js

--- mindfulKnight.js
+++ mindfulKnight.js
@@ -21,11 +21,20 @@ router.use(express.json());
 
 
 router.get('/', readHelloMessage);
+
+router.post('/users', createUser);
+
+router.get('/users/email/:email', readUserFromEmail);
+
+
+router.get('/users/:email', readUser);
+
+
 router.get('/users', readUsers);
 router.get('/fidget_toys', readFidgetToys);
 router.get('/user_activity', readUserActivity);
 router.get('/user_activity/:id', readUserActivityById);
-router.post('/user_activity', createUserActivity);
+//router.post('/user_activity', createUserActivity);
 router.put('/user_activity/:id', updateUserActivity);
 router.delete('/user_activity/:id', deleteUserActivity);
 
@@ -60,6 +69,82 @@ function readUsers(req, res, next) {
 }
 
 
+// function createUser(req, res, next) {
+//   const { name, email, password } = req.body;
+//   db.one('INSERT INTO users(name, emailAddress, password_hash, registration_date) VALUES (${name}, ${email}, ${password}, CURRENT_TIMESTAMP) RETURNING *', {
+//       name,
+//       email,
+//       password,
+//     })
+//     .then((data) => {
+//       res.send(data);
+//     })
+//     .catch((err) => {
+//       next(err);
+//     });
+// }
+
+
+
+
+function createUser(req, res, next) {
+  console.log('Received data from client:', req.body);
+
+  db.one('INSERT INTO users(username, email, password_hash, registration_date) VALUES (${username}, ${email}, ${password_hash}, NOW()) RETURNING user_id', req.body)
+    .then((data) => {
+      console.log('Inserted user with ID:', data.user_id);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.error('Error inserting user:', err);
+      next(err);
+    });
+}
+
+
+
+
+
+
+function readUserFromEmail(req, res, next) {
+  db.oneOrNone("SELECT * FROM Users WHERE email='" + req.params.email + "'", req.params)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+function readUser(req, res, next) {
+  const email = req.params.email.toLowerCase(); // Convert the email to lowercase
+
+  db.oneOrNone('SELECT * FROM Users WHERE LOWER(email) = $1', [email])
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+
+
+
+
+
+
+
+
 function readFidgetToys(req, res, next) {
   db.many('SELECT * FROM fidget_toys')
     .then((data) => {
@@ -92,6 +177,8 @@ function readUserActivityById(req, res, next) {
 }
 
 function createUserActivity(req, res, next) {
+  console.log('Request Body:', req.body);  // Log the request body
+
   db.one('INSERT INTO user_activity(user_id, toy_id, start_time, end_time) VALUES (${user_id}, ${toy_id}, ${start_time}, ${end_time}) RETURNING activity_id', req.body)
     .then((data) => {
       res.send(data);
@@ -121,5 +208,18 @@ function deleteUserActivity(req, res, next) {
     });
 }
 
+// Add a new route for updating time spent on the app
+router.put('/update_time', updateTimeSpent);
 
+function updateTimeSpent(req, res, next) {
+  const { userId, elapsedTime } = req.body;
 
+  // Update the user's time spent on the app
+  db.none('UPDATE users SET time_spent = time_spent + $1 WHERE user_id = $2', [elapsedTime, userId])
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
